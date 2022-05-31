e3717cdfc236b9510a0866c1224603bcbd0a0a66
dabaisuv <dabaisuv@gmail.com>
2022-05-31 21:37:10 +0800

    Update Unlimited_downloader.js

--- Unlimited_downloader.js
+++ Unlimited_downloader.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Unlimited_downloader
 // @namespace    ooooooooo.io
-// @version      0.1.4
+// @version      0.1.5
 // @description  Get video and audio binary streams directly, breaking all download limitations. (As long as you can play, then you can download!)
 // @author       dabaisuv
 // @match        *://*/*
@@ -47,8 +47,8 @@
          window.video = [];
          console.log('video array cleared.');
       }
-      var sourceBuffer = _addSourceBuffer.call(this, mime)
-      var _append = sourceBuffer.appendBuffer
+      let sourceBuffer = _addSourceBuffer.call(this, mime)
+      const _append = sourceBuffer.appendBuffer
       sourceBuffer.appendBuffer = function (buffer) {
          console.log(mime, buffer);
          if (mime.toString().indexOf('audio') !== -1) {
@@ -58,43 +58,60 @@
          }
          _append.call(this, buffer)
       }
+
+      sourceBuffer.appendBuffer.toString = function () {
+         console.log('appendSourceBuffer hook is detecting!');
+         return _append.toString();
+      }
       return sourceBuffer
+   }
+
+   window.MediaSource.prototype.addSourceBuffer.toString = function () {
+      console.log('addSourceBuffer hook is detecting!');
+      return _addSourceBuffer.toString();
+   }
+
+   function download() {
+      let a = document.createElement('a');
+      a.href = window.URL.createObjectURL(new Blob(window.audio));
+      a.download = document.title + '_audio.mp4';
+      a.click();
+      a.href = window.URL.createObjectURL(new Blob(window.video));
+      a.download = document.title + '_video.mp4';
+      a.click();
+      window.downloadAll = 0
+
+
+      // window.open(window.URL.createObjectURL(new Blob(window.audio)));
+      // window.open(window.URL.createObjectURL(new Blob(window.video)));
+      // window.downloadAll = 0
+
+      // GM_download(window.URL.createObjectURL(new Blob(window.audio)));
+      // GM_download(window.URL.createObjectURL(new Blob(window.video)));
+      // window.isComplete = 0;
+
+      // const { createFFmpeg } = FFmpeg;
+      // const ffmpeg = createFFmpeg({ log: true });
+      // (async () => {
+      //     const { audioName } = new File([new Blob(window.audio)], 'audio');
+      //     const { videoName } = new File([new Blob(window.video)], 'video')
+      //     await ffmpeg.load();
+      //     //ffmpeg -i audioLess.mp4 -i sampleAudio.mp3 -c copy output.mp4
+      //     await ffmpeg.run('-i', audioName, '-i', videoName, '-c', 'copy', 'output.mp4');
+      //     const data = ffmpeg.FS('readFile', 'output.mp4');
+      //     let a = document.createElement('a');
+      //     let blobUrl = new Blob([data.buffer], { type: 'video/mp4' })
+      //     console.log(blobUrl);
+      //     a.href = URL.createObjectURL(blobUrl);
+      //     a.download = 'output.mp4';
+      //     a.click();
+      // })()
+      // window.downloadAll = 0;
    }
 
    setInterval(() => {
       if (window.downloadAll === 1) {
-         let a = document.createElement('a');
-         a.href = window.URL.createObjectURL(new Blob(window.audio));
-         a.download = 'audio.mp4';
-         a.click();
-         a.href = window.URL.createObjectURL(new Blob(window.video));
-         a.download = 'video.mp4'
-         a.click();
-         window.downloadAll = 0
-
-
-         // window.open(window.URL.createObjectURL(new Blob(window.audio)));
-         // window.open(window.URL.createObjectURL(new Blob(window.video)));
-         // window.downloadAll = 0
-
-
-         // const { createFFmpeg } = FFmpeg;
-         // const ffmpeg = createFFmpeg({ log: true });
-         // (async () => {
-         //     const { audioName } = new File([new Blob(window.audio)], 'audio');
-         //     const { videoName } = new File([new Blob(window.video)], 'video')
-         //     await ffmpeg.load();
-         //     //ffmpeg -i audioLess.mp4 -i sampleAudio.mp3 -c copy output.mp4
-         //     await ffmpeg.run('-i', audioName, '-i', videoName, '-c', 'copy', 'output.mp4');
-         //     const data = ffmpeg.FS('readFile', 'output.mp4');
-         //     let a = document.createElement('a');
-         //     let blobUrl = new Blob([data.buffer], { type: 'video/mp4' })
-         //     console.log(blobUrl);
-         //     a.href = URL.createObjectURL(blobUrl);
-         //     a.download = 'output.mp4';
-         //     a.click();
-         // })()
-         // window.downloadAll = 0;
+         download();
       }
    }, 2000);
 
@@ -109,23 +126,7 @@
       let autoDownInterval = setInterval(() => {
          //document.querySelector('video').playbackRate = 16.0;
          if (window.isComplete === 1) {
-            let a = document.createElement('a');
-            a.href = window.URL.createObjectURL(new Blob(window.audio));
-            a.download = 'audio.mp4';
-            a.click();
-            a.href = window.URL.createObjectURL(new Blob(window.video));
-            a.download = 'video.mp4'
-            a.click();
-            window.isComplete = 0;
-
-            // window.open(window.URL.createObjectURL(new File([new Blob(window.audio)], 'audio.mp4')));
-            // window.open(window.URL.createObjectURL(new File([new Blob(window.video)], 'video.mp4')));
-            // window.isComplete = 0;
-
-            // GM_download(window.URL.createObjectURL(new Blob(window.audio)));
-            // GM_download(window.URL.createObjectURL(new Blob(window.video)));
-            // window.isComplete = 0;
-
+            download();
          }
       }, 2000);
    }
